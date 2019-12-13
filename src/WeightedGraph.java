590a9c675c02283c0b358dfa8187c530d8e539eb
HoldenGjuka <53842219+HoldenGjuka@users.noreply.github.com>
2019-12-12 21:32:16 -0800

    More progress on RunPrims()

--- src/WeightedGraph.java
+++ src/WeightedGraph.java
@@ -73,9 +73,10 @@ public class WeightedGraph {
      */
     public List<Edge> runPrims() {
         ArrayList<Edge> mst = new ArrayList<>();
-        Set<Integer> vertices = graph.keySet();
+        HashSet<Integer> verticesInMST = new HashSet<>();
+        Set<Integer> allVertices = graph.keySet();
         System.out.println("Graph: " + graph.toString());
-        System.out.println("Vertices: " + vertices);
+        System.out.println("All Vertices: " + allVertices);
         EdgePriorityQueue epq = new EdgePriorityQueue();
         int arbitraryVertex = 0;
         //get all edges connected to that edge
@@ -88,8 +89,11 @@ public class WeightedGraph {
         }
         Edge firstEdge = epq.remove();
         mst.add(firstEdge);
-        int verticesInMST = 1;
-        for (int i = verticesInMST; i < vertices.size() - 1; i++) {
+        verticesInMST.add(arbitraryVertex);
+        verticesInMST.add(firstEdge.getExternalVertex());
+        int numVertsInMST = 1;
+        for (int i = numVertsInMST; i < allVertices.size() - 1; i++) {
+            System.out.println("Vertices in MST: " + verticesInMST.toString());
             System.out.println("i = " + i);
             Edge priorEdge = mst.get(i - 1);
             int v = priorEdge.getExternalVertex();
@@ -98,16 +102,19 @@ public class WeightedGraph {
             for (int j = 0; j < adjacencies.size() - 1; j++) {
                 Adjacency a = adjacencies.get(j);
                 Edge e = new Edge(a.vertex, arbitraryVertex, a.weight);
-                if(true) {
+                if(!verticesInMST.contains(priorEdge.getExternalVertex()) && !verticesInMST.contains(v)) {
                     System.out.println("Added edge to EPQ: " + e);
                     epq.decrease(e.getExternalVertex(), v, e.getWeight());
                     epq.add(e);
                 }
             }
-            mst.add(epq.remove());
+            Edge newEdge = epq.remove();
+            mst.add(newEdge);
+            verticesInMST.add(newEdge.getExternalVertex());
         }
         System.out.println("MST: " + mst);
         System.out.println("EPQ: " + epq.toString());
         return null;
     }
+
 }
